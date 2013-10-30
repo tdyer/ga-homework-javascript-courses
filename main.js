ccc1a766627f1363b07576b87555e8f782c58718
Abby Howell <abby.g.howell@gmail.com>
2013-10-30 17:53:32 -0400

    making functions

--- main.js
+++ main.js
@@ -21,22 +21,26 @@ CourseApp.Teacher.prototype.generate_html = function(){
 // =========== Course ==============
 CourseApp.Course = function(name) {
   this.name = name;
-  this.teachers = [];
   this.students = [];
 }
 
 // This will generate *all* of the html for a specific course
 CourseApp.Course.prototype.generate_html = function(){
-	return "<h1> Course: " + this.name + "</h1>";
+	return "<h1> Course: " + this.name + ", teacher: " + this.teacher.name + "</h1><ul class='" + this.name + " student-list'></ul>";
+	var students-list = document.getElementsByClassName(this.name 'student-list')[0];
+	x = 0,
+	max = this.students.length;
+	for (; x < max; ){
+    student-list.innerHTML += "<li>" + this.students[x].name + "</li>"
+	};
 };
 CourseApp.Course.prototype.add_teacher = function(teacher){
 	var name = prompt("Teacher name");
-	new_teacher = new Teacher(name);
-	this.teachers.push(new_teacher);
+	this.teacher = new Teacher(name);
 };
 CourseApp.Course.prototype.add_student = function(student){
   var name = prompt("Student name");
-  new_student = new Student(name);
+  var new_student = new Student(name);
   this.students.push(new_student);
 }
 
@@ -49,9 +53,15 @@ CourseApp.Course.prototype.add_student = function(student){
 
 // Generate the HTML for this course
 
-CourseApp.course_names = ['psych', 'algebra'];
+CourseApp.course_names = ['psych', 'algebra', 'french lit', 'geometry', 'world history'];
 CourseApp.add_course = function(e){
   // Add a course,can get the names from course_names above!
+  e = 0,
+  max = CourseApp.course_names.length;
+  for (; e < max ; ) {
+  	var new_course = new Course(course_names[e]);
+  	e += 1;
+  }
 }
 
 CourseApp.student_names = ['bob', 'alice', 'tom', 'mike', 'joe', 'frank', 'ed'];
