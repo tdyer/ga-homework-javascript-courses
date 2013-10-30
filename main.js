594ba1e791b23bfbcf4aaa7c24b0a6530a49408c
Abby Howell <abby.g.howell@gmail.com>
2013-10-30 18:06:18 -0400

    brain melting

--- main.js
+++ main.js
@@ -15,19 +15,19 @@ CourseApp.Teacher = function(name) {
   this.name = name;
 }
 CourseApp.Teacher.prototype.generate_html = function(){
-	return "<li>" + this.name + "</li>";
+	return "<h3> Teacher: " + this.name + "</h3>";
 };
 
 // =========== Course ==============
 CourseApp.Course = function(name) {
   this.name = name;
   this.students = [];
-}
+};
 
 // This will generate *all* of the html for a specific course
 CourseApp.Course.prototype.generate_html = function(){
-	return "<h1> Course: " + this.name + ", teacher: " + this.teacher.name + "</h1><ul class='" + this.name + " student-list'></ul>";
-	var students-list = document.getElementsByClassName(this.name 'student-list')[0];
+	"<h1> Course: " + this.name + "</h1><ul class='" + this.name + "></ul>";
+	var students_list = document.getElementsByClassName(this.name)[0];
 	x = 0,
 	max = this.students.length;
 	for (; x < max; ){
@@ -42,7 +42,7 @@ CourseApp.Course.prototype.add_student = function(student){
   var name = prompt("Student name");
   var new_student = new Student(name);
   this.students.push(new_student);
-}
+};
 
 
 // Create a course
@@ -52,19 +52,17 @@ CourseApp.Course.prototype.add_student = function(student){
 // Create a couple of students and add it to the above course.
 
 // Generate the HTML for this course
-
+CourseApp.courses = [];
 CourseApp.course_names = ['psych', 'algebra', 'french lit', 'geometry', 'world history'];
 CourseApp.add_course = function(e){
   // Add a course,can get the names from course_names above!
-  e = 0,
-  max = CourseApp.course_names.length;
-  for (; e < max ; ) {
-  	var new_course = new Course(course_names[e]);
-  	e += 1;
-  }
-}
+  var new_course = new Course(prompt("Course Name"));
+  new_course.add_teacher();
+  new_course.generate_html();
+};
 
 CourseApp.student_names = ['bob', 'alice', 'tom', 'mike', 'joe', 'frank', 'ed'];
 CourseApp.add_student = function(e){
-  // Add a student, can get the names from student_names above!
+  var new_student = new Student(prompt("Student name"));
+  
 }
