86d35e9a3971b63aaa373ed43b5c6bf86ecd3c79
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-30 23:07:59 -0400

    bonus prompts working

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Application namespace
 var CourseApp = {};
-
+var students = [];
 // =========== Student ==============
 CourseApp.Student = function(name) {
                  // a student object properties
   this.name = name;
+  students.push(this);
 };
 
 p1 = new CourseApp.Student('Mike');
 p2 = new CourseApp.Student('Amal');
 p3 = new CourseApp.Student('Emma');
 p4 = new CourseApp.Student('Matt');
-var students = [p1, p2, p3, p4];
 
 CourseApp.Student.prototype.generate_html = function(students){
     var studentsHTML = document.getElementById('students').innerHTML;
@@ -25,18 +25,18 @@ CourseApp.Student.prototype.generate_html = function(students){
     document.getElementById('students').innerHTML = studentsHTML;
 };
 
-CourseApp.Student.prototype.generate_html(students);
-
+var teachers = [];
 // =========== Teacher ==============
 CourseApp.Teacher = function(name) {
                  // a teacher object properties
                  this.name = name;
+                 teachers.push(this);
 };
 
 t1 = new CourseApp.Teacher('David');
 t2 = new CourseApp.Teacher('Tom');
 t3 = new CourseApp.Teacher('Teddy');
-var teachers = [t1, t2, t3];
+
 
 CourseApp.Teacher.prototype.generate_html = function(teachers){
    var teachersHTML = document.getElementById('teachers').innerHTML;
@@ -50,18 +50,19 @@ CourseApp.Teacher.prototype.generate_html = function(teachers){
    document.getElementById('teachers').innerHTML = teachersHTML;
 };
 
-CourseApp.Teacher.prototype.generate_html(teachers);
 
 // // =========== Course ==============
+var courses = [];
 CourseApp.Course = function(name) {
                  // a course object properties
   this.name = name;
+  courses.push(this);
 };
 
 c1 = new CourseApp.Course('Front End');
 c2 = new CourseApp.Course('WDI');
 c3 = new CourseApp.Course('UX/UI');
-var courses = [c1,c2,c3];
+
 
 CourseApp.Course.prototype.generate_html = function(courses){
     var coursesHTML = document.getElementById('courses').innerHTML;
@@ -75,18 +76,42 @@ CourseApp.Course.prototype.generate_html = function(courses){
     document.getElementById('courses').innerHTML = coursesHTML;
 }
 
+///////////////////////////////////////////////////////////
+//                                                       //
+//            BONUS STAGE BELOW                          //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+
+// The following 3 methods will run ONLY when you click the 
+// 'Add Foo' button in the HTML. Ignore what the 'e' is in each
+// of the methods
+
+// For example, if I click the 'Add Teacher' button on the page
+// the 'CourseApp.add_teacher' method will run
+
+CourseApp.add_teacher = function(e){
+  var name = prompt("What's the teacher's name?")
+  new_teacher = new CourseApp.Teacher(name);
+}
+
+CourseApp.add_course = function(e){
+  var name = prompt("What's the course name?")
+  new_course = new CourseApp.Course(name);
+}
+
+CourseApp.add_student = function(e){
+  var name = prompt("What's the student's name?")
+  new_student = new CourseApp.Student(name);
+}
+
+CourseApp.add_teacher();
+CourseApp.Teacher.prototype.generate_html(teachers);
+CourseApp.add_course();
 CourseApp.Course.prototype.generate_html(courses);
+CourseApp.add_student();
+CourseApp.Student.prototype.generate_html(students);
 
-/////// BONUS ////////
-
-// This will generate *all* of the html for a specific course
-
-// CourseApp.Course.prototype.add_teacher = function(teacher){ ...}
-// CourseApp.Course.prototype.add_student = function(student){ ...}
-// CourseApp.add_course = function(e){
-//   // Prompt the user for information to add a course
-//   // Append this course to the list of courses on the page
-// }
 
 
 
