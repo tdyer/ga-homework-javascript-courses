c0e532595db2947b60fdaf9aea74e98d74c49f49
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-30 20:23:01 -0400

    Students methods built

--- main.js
+++ main.js
@@ -1,44 +1,54 @@
-// Application namespace
 var CourseApp = {};
 
 // =========== Student ==============
 CourseApp.Student = function(name) {
-                 // a student object properties
-}
-CourseApp.Student.prototype.generate_html = function(){ ...}
+	this.name = name;
+};
+
+CourseApp.Student.prototype.generate_html = function(){
+    return "<li id='" + this.name + "' class='student'><h4>" + this.name + "</h4></li>";
+};
 
 
-// =========== Teacher ==============
-CourseApp.Teacher = function(name) {
-                 // a teacher object properties
-}
-CourseApp.Teacher.prototype.generate_html = function(){ ...}
+// // =========== Teacher ==============
+// CourseApp.Teacher = function(name) {
+//                  // a teacher object properties
+// };
 
-// =========== Course ==============
-CourseApp.Course = function(name) {
-                 // a course object properties
-}
+// CourseApp.Teacher.prototype.generate_html = function(){
 
-// This will generate *all* of the html for a specific course
-CourseApp.Course.prototype.generate_html = function(){ ...}
-CourseApp.Course.prototype.add_teacher = function(teacher){ ...}
-CourseApp.Course.prototype.add_student = function(student){ ...}
+// };
+
+// // =========== Course ==============
+// CourseApp.Course = function(name) {
+//                  // a course object properties
+// }
+
+// // This will generate *all* of the html for a specific course
+// CourseApp.Course.prototype.generate_html = function(){ 
+// }
+
+// CourseApp.Course.prototype.add_teacher = function(teacher){ 
+// }
+
+// CourseApp.Course.prototype.add_student = function(student){ 
+// }
 
 
-// Create a course
+// // Create a course
 
-// Create a teacher and add it to the above course.
+// // Create a teacher and add it to the above course.
 
-// Create a couple of students and add it to the above course.
+// // Create a couple of students and add it to the above course.
 
-// Generate the HTML for this course
+// // Generate the HTML for this course
 
-CourseApp.course_names = ['psych', 'algebra'];
-CourseApp.add_course = function(e){
-  // Add a course,can get the names from course_names above!
-}
+// CourseApp.course_names = ['psych', 'algebra'];
+// CourseApp.add_course = function(e){
+//   // Add a course,can get the names from course_names above!
+// }
 
-CourseApp.student_names = ['bob', 'alice', 'tom', 'mike', 'joe', 'frank', 'ed'];
-CourseApp.add_student = function(e){
-  // Add a student, can get the names from student_names above!
-}
+// CourseApp.student_names = ['bob', 'alice', 'tom', 'mike', 'joe', 'frank', 'ed'];
+// CourseApp.add_student = function(e){
+//   // Add a student, can get the names from student_names above!
+// }
